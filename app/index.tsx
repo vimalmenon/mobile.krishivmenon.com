2c4e1d76a844cabfcf55efb8a61e139e9f52c8a7
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-07-08 18:37:27 +0800

    Working on style

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { mainStyle } from "../src/style/main.style";
 
-const App:React.FC = () => {
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={mainStyle.container}>
+      <Text>This is website for Krishiv Menon</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 export default App
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/style/main.style.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const mainStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
