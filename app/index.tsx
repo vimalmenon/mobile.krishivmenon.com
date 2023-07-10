33b67a43716ba4f2a73f79406a6320a2e0dd2a3d
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-07-10 15:32:06 +0800

    Package Updated

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { mainStyle } from "../src/style/main.style";
+import { PaperProvider, Button } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
+type RootStackParamList = {
+  Home: {
+    id: number,
+  }
+};
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const HomeScreen: React.FC<Props> = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
 const App: React.FC = () => {
   return (
-    <View style={mainStyle.container}>
-      <Text>This is the App for Krishiv Menon</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <PaperProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} initialParams={{ id: 1 }} />
+        </Stack.Navigator>
+        <View style={mainStyle.container}>
+          <Text>This is the App for Krishiv Menon</Text>
+          <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+            Press me
+          </Button>
+        </View>
+
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
 
