93d6cea4411cc164db8bae43f3b1146cf2b39857
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-07-25 07:26:58 +0800

    Working on Note detail

--- src/pages/NoteDetail.tsx
+++ src/pages/NoteDetail.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { INotes, RootStackParamList } from '@types';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { Button, TextInput } from 'react-native-paper';
 
 import { Layout } from './Layout';
@@ -16,12 +16,17 @@ export const NoteDetail: React.FC<NoteDetailProps> = ({ route }) => {
   };
   return (
     <Layout page={route.name}>
-      <View className="flex-1 p-3">
-        <View className="flex">
+      <View className="flex-1 p-3 gap-2">
+        <View className="flex my-1">
           <TextInput value={note.title} onChangeText={(value) => onChange('title', value)} />
         </View>
-        <View className="flex-1">
-          <Text>{note.content}</Text>
+        <View className="flex-1 my-1">
+          <TextInput
+            value={note.content}
+            onChangeText={(value) => onChange('content', value)}
+            multiline
+            className="flex-1"
+          />
         </View>
         <View className="flex flex-row justify-between">
           <Button mode="contained">Save</Button>
